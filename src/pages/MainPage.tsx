159fdb2b88259a6b113ae990aca9891e33789782
Dasha <dasha@peoplcat.com>
2023-12-27 16:54:16 +0900

    init

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,8 @@
+export default function MainPage() {
+  return (
+    <div>
+      <h2>MainPage</h2>
+      <button type="button">바로가기1</button>
+    </div>
+  );
+}
---
+++ src/pages/Routers.tsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./MainPage";
+
+export default function Routers() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  );
+}
