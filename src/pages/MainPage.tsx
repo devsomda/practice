35d5ce5e9fb92c5781f8d83cbde46fe5fc2605a8
Dasha <dasha@peoplcat.com>
2023-12-27 19:07:33 +0900

    apply react-router-dom

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,7 +2,7 @@ export default function MainPage() {
   return (
     <div>
       <h2>MainPage</h2>
-      <button type="button">바로가기1</button>
+      <a href="/study/css-pseudo-class">바로가기1</a>
     </div>
   );
 }
--- src/pages/Routers.tsx
+++ src/pages/Routers.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./MainPage";
+import CssPseudoClass from "./CssPseudoClass";
 
 export default function Routers() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/study/css-pseudo-class" element={<CssPseudoClass />} />
     </Routes>
   );
 }
