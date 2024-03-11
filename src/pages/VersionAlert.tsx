3cd0301fdb55f2bc0b221a4ac77135ba59dc56f2
Dasha <dasha@peoplcat.com>
2024-03-11 17:48:17 +0900

    practice: add state change button

--- src/pages/VersionAlert.tsx
+++ src/pages/VersionAlert.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import packageJson from "../../package.json";
 
 export default function VersionAlert() {
+  const [state, setState] = useState("");
+
   const version = window.localStorage.getItem("version");
 
   useEffect(() => {
-    if (!version) {
+    if (!version || version !== packageJson.version) {
       window.localStorage.setItem("version", packageJson.version);
     }
     if (version !== packageJson.version) {
@@ -15,17 +17,26 @@ export default function VersionAlert() {
     console.log("local ver", version, "current", packageJson.version);
   }, []);
 
-  const actionTrigger = () => {
+  const refreshTrigger = () => {
     window.location.reload();
+  };
+
+  const stateChangeHandler = () => {
+    const date = new Date();
+    setState(`State Changed at ${date}`);
   };
 
   return (
     <div>
       <p>LocalStorage Verion: {version}</p>
       <p>Package Version: {packageJson.version}</p>
-      <button type="button" onClick={actionTrigger}>
+      <button type="button" onClick={refreshTrigger}>
         refresh action trigger
       </button>
+      <button type="button" onClick={stateChangeHandler}>
+        state change event
+      </button>
+      <p>State: {state}</p>
     </div>
   );
 }
